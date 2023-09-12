f0b13187d3ed30fccc62a265dfd96b9ceb18d2e2
mikasackermn <mikasa@rango.exchange>
2023-09-12 17:00:13 +0330

    update

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,17 @@
-import '../styles/globals.css'
+import '../styles/globals.scss'
 import type { AppProps } from 'next/app'
+import NextNProgress from 'nextjs-progressbar';
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <NextNProgress
+        color="#00A9BB"
+        height={2}
+        showOnShallow={true}
+      />
+
+      <Component {...pageProps} />
+    </>
+  )
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,53 +1,33 @@
 module.exports = {
   content: [
     './pages/**/*.{js,ts,jsx,tsx}',
-    './components/**/*.{js,ts,jsx,tsx}',
+    './components/**/*.{js,ts,jsx,tsx}'
   ],
+  safelist: ['text-success', 'bg-success', 'border-success'],
   theme: {
     fontFamily: {
       sans: ['Aileron'],
-      heading: ['impactreg'],
+      heading: ['impactreg']
     },
     extend: {
+      visibility: ['group-hover'],
+
       colors: {
-        primary: '#5fa425',
+        primary: '#00A9BB',
         secondary: '#da606c',
-        neutralDarkest: '#000',
-        neutralLightest: '#ffffff',
-        gray: 'rgba(255, 255, 255, 0.6)',
-      },
-      keyframes: {
-        ['bounce-slow']: {
-          '0%': {
-            transform: 'scale(1,1) translateY(-400%)',
-          },
-          '10%': {
-            transform: 'scale(1.1,0.9) translateY(0)',
-          },
-          '30%': {
-            transform: 'scale(0.9,1.1) translateY(25%)',
-          },
-          '50%': {
-            transform: 'scale(1.05,0.95) translateY(0)',
-          },
-          '62.5%': {
-            transform: 'scale(1,1) translateY(-5%)',
-          },
-          '75%': {
-            transform: 'scale(1,1) translateY(0)',
-          },
-          '87.5%': {
-            transform: 'scale(1,1) translateY(2%)',
-          },
-          '100%': {
-            transform: 'scale(1,1) translateY(0)',
-          },
-        },
-      },
-      animation: {
-        ['bounce-slow']: 'bounce 1s infinite, bounce-slow 1.5s ease-in-out',
-      },
+        background: '#ECF3F4',
+        error: '#F40000',
+        failed: '#F40000',
+        warning: '#ef9b0f',
+        success: '#0AA65B',
+        neutral: {
+          100: '#FFFFFF',
+          300: '#D0D0D0',
+          600: '#636363',
+          900: '#000000'
+        }
+      }
     },
-    plugins: [],
-  },
+    plugins: []
+  }
 }
